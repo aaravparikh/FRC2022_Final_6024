bbd8ea4c742d5ebcaa14ae212fc9f5799426ac99
NxtGenLegend <62510055+NxtGenLegend@users.noreply.github.com>
2022-03-17 00:01:48 +0530

    Update DriveCommand.java

---
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class DriveCommand extends CommandBase {
+    
+    private DriveSubsystem robotDrive;
+    private double leftspeed;
+    private double rightspeed;
+
+    public DriveCommand(DriveSubsystem robotDrive, double leftspeed, double rightspeed){
+        this.robotDrive = robotDrive;
+        this.leftspeed = leftspeed;
+        this.rightspeed = rightspeed;
+        addRequirements(robotDrive);
+    }
+
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public void execute(){
+        robotDrive.setSpeed(leftspeed, rightspeed);
+    }
+
+    @Override
+    public void end (boolean interrupted){
+        robotDrive.setSpeed(0, 0);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+}
